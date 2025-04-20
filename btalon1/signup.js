ba69e6245804b6113fd6e5ed4778d0ae57899fbb
kanhrcuoihaha <nduckanhh145@gmail.com>
2025-04-20 20:47:45 +0700

    Add files via upload

---
+++ btalon1/signup.js
@@ -0,0 +1,26 @@
+// validation form register and register user local storage
+const inputUsernameRegister = document.querySelector(".username-signup");
+const inputPasswordRegister = document.querySelector(".password-signup");
+const btnRegister = document.querySelector(".send-signup");
+
+// validation form register and register user local storage
+
+btnRegister.addEventListener("click", (e) => {
+  e.preventDefault();
+  if (
+    inputUsernameRegister.value === "" ||
+    inputPasswordRegister.value === ""
+  ) {
+    alert("vui lòng không để trống");
+  } else {
+    // array user
+    const user = {
+      username: inputUsernameRegister.value,
+      password: inputPasswordRegister.value,
+    };
+    let json = JSON.stringify(user);
+    localStorage.setItem(inputUsernameRegister.value, json);
+    alert("Đăng Ký Thành Công");
+    window.location.href = "login.html";
+  }
+});
